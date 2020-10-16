46c79fa51f2b49c377004c2c662a8156b4c2856d
winstonbarbe <winston.barbe@gmail.com>
2020-10-16 10:29:33 -0400

    Adds crud routes on home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,138 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Movies</h1>
+    <div>
+      <h2>Add Movie</h2>
+      Title: <input v-model="newMovieTitle" type="text"><br><br>
+      Year: <input v-model="newMovieYear" type="text"><br><br>
+      Plot: <input v-model="newMoviePlot" type="text"><br><br>
+      Director: <input v-model="newMovieDirector" type="text"><br><br>
+      <h4>Choose Genres:</h4> 
+      <input type="checkbox" id="drama" value="1" v-model="newMovieGenre">
+      <label for="drama">Drama</label>
+      <input type="checkbox" id="comedy" value="2" v-model="newMovieGenre">
+      <label for="comedy">Comedy</label>
+      <input type="checkbox" id="western" value="3" v-model="newMovieGenre">
+      <label for="western">Western</label>
+      <input type="checkbox" id="thriller" value="4" v-model="newMovieGenre">
+      <label for="thriller">Thriller</label>
+      <input type="checkbox" id="action" value="5" v-model="newMovieGenre">
+      <label for="action">Action</label>
+      
+      <!-- <input v-model="newMovieEnglish" type="checkbox" id="english" 
+         checked>
+      <label  for="english">english</label><br> -->
+     <button v-on:click="createMovie()">Add</button>
+   </div>
+
+    <div v-for="movie in movies">
+      <h3>{{ movie.title }}</h3>
+      <h4>Genres:</h4>
+      <div v-for="genre in movie.genres">
+        <p>{{ genre }}</p>
+      </div>
+      <p>Year: {{ movie.year }}</p>
+      <p>Director: {{ movie.director }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <button v-on:click="showMovie(movie)">More Info</button>
+    </div>
+
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h2>Movie Info</h2>
+        {{ currentMovie }}
+        <p>Title: <input type="text" v-model="currentMovie.title"></p>
+        <p>Year: <input type="text" v-model="currentMovie.year"></p>
+        <p>Director: <input type="text" v-model="currentMovie.director"></p>
+        <p>Plot: <input type="text" v-model="currentMovie.plot"></p>
+        <button v-on:click="updateMovie(currentMovie)">Update</button>
+        <button v-on:click="deleteMovie(currentMovie)">delete</button>
+        <button>Close</button>
+      </form>
+    </dialog>
+      
+    
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      movies: [],
+      newMovieDirector: "",
+      newMoviePlot: "",
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMovieEnglish: true,
+      newMovieGenre: [],
+      currentMovie: {}
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/api/movies").then( response => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        director: this.newMovieDirector,
+        plot: this.newMoviePlot,
+        english: this.newMovieEnglish,
+        genres: this.newMovieGenre
+      };
+      axios.post("api/movies", params).then(response => {
+        console.log("Success", response.data);
+        this.movies.push(response.data);
+        this.newMovieDirector = "";
+        this.newMovieTitle = "";
+        this.newMoviePlot = "";
+        this.newMovieYear = "";
+        this.newMovieGenre = [];
+
+      }).catch(error => {
+        console.log(error.response.data.errors);
+      });
+      
+    },
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+    updateMovie: function (movie) {
+      var params = {
+        title: movie.title,
+        plot: movie.plot,
+        year: movie.year,
+        director: movie.director
+      };
+      axios.patch(`/api/movies/${movie.id}`, params).then( response => {
+        console.log("Success", response.data);
+      }).catch(error => {
+        console.log(error.response.data.errors);
+      });
+    },
+    destroyRecipe: function (movie) {
+      axois.delete(`/api/movies/${movie.id}`).then(response => {
+        console.log("Success", response.data);
+        var index = this.movies.indexOf(movie);
+        this.movies.splice(index, 1);
+
+      });
+    }
   }
-}
+};
 </script>
